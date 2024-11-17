ef08059015ccef5b02520ecee1c11fc3d33c86e6
Henning Ottesen <henning@live.no>
2024-11-17 15:06:21 +0100

    Add clap

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,24 +1,43 @@
+use std::path::PathBuf;
+
 use chip_8_core::Chip8Builder;
+use clap::Parser;
+
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Filepath to Chip-8 ROM file that will be executed
+    #[clap(index = 1)]
+    rom: PathBuf,
+
+    /// Print debug information
+    #[clap(short, long)]
+    debug: bool,
+}
 
 fn main() {
-        // Load rom
-        let rom = std::fs::read("roms/ibm-logo.ch8").unwrap();
+    let args = Args::parse();
 
-        let mut chip = Chip8Builder::new()
-            .with_rom(&rom[..])
-            .with_debug(true)
-            .build();
-    
-        for _ in 0..25 {
-            chip.step();
+    // Load rom
+    let rom = std::fs::read(args.rom).unwrap();
+
+    let mut chip = Chip8Builder::new()
+        .with_rom(&rom[..])
+        .with_debug(true)
+        .build();
+
+    for _ in 0..25 {
+        chip.step();
+    }
+
+    for y in 0..32 {
+        for x in 0..64 {
+            let i = y * 64 + x;
+            let pxl = if chip.display[i] > 0 {'█'} else {' '};
+            print!("{}", pxl);
         }
-    
-        for y in 0..32 {
-            for x in 0..64 {
-                let i = y * 64 + x;
-                let pxl = if chip.display[i] > 0 {'█'} else {' '};
-                print!("{}", pxl);
-            }
-            println!();
-        }
+        println!();
+    }
 }
